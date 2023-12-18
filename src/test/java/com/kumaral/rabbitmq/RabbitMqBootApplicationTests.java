ae78f7bba1e7f99bccb7f3b7d69b37375f78bc9c
Alok Kumar <alok27may@gmail.com>
2023-12-18 12:42:13 +0530

    adding first commit

---
+++ src/test/java/com/kumaral/rabbitmq/RabbitMqBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumaral.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
